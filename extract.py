ed59ac7a8cec1a57d761fd64f52f9044bb4734c6
ghost-mann <iamaob8@protonmail.com>
2025-05-19 18:10:29 +0300

    initial commit

---
+++ extract.py
@@ -0,0 +1,15 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+def fetch_xauusd_data():
+    url = f"https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=5min&apikey={API_KEY}"
+    return requests.get(url).json()
+
+if __name__ == "__main__":
+    data = fetch_xauusd_data()
+    print(data)
